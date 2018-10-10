af174d3252b8eef2bbd0e59b8ff3aa5d1d22f17b
andrewquamme <andrewquamme@gmail.com>
2018-10-09 17:13:47 -0700

    Fixes console logs

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ if (answerTwo === 'YES' || answerTwo === 'Y') {
   console.log('Question one was answered correcty');
 } else {
   alert('Incorrect, ' + username + '. I enlisted into the Army in 2000.');
-  console.log('Question one was answered incorrectly');
+  console.log('Question two was answered incorrectly');
 }
 
 /* Question Three
@@ -44,7 +44,7 @@ if (answerThree === 'YES' || answerThree === 'Y') {
   console.log('Question one was answered correcty');
 } else {
   alert('Unfortunately, ' + username + ', I was there for two years (04-05 and again from 05-06).');
-  console.log('Question one was answered incorrectly');
+  console.log('Question three was answered incorrectly');
 }
 
 /* Question Four
@@ -57,7 +57,7 @@ if (answerFour === 'YES' || answerFour === 'Y') {
   console.log('Question one was answered incorrecty');
 } else {
   alert('100% correct, ' + username + '. I do not like it at all!');
-  console.log('Question one was answered correctly');
+  console.log('Question four was answered correctly');
 }
 
 /* Question Five
@@ -67,7 +67,7 @@ var answerFive = prompt('Have I cycled from Seattle to Portland?').toUpperCase()
 console.log(answerFive);
 if (answerFive === 'YES' || answerFive === 'Y') {
   alert('Nice job, ' + username + '! Over 200 miles!');
-  console.log('Question one was answered correcty');
+  console.log('Question five was answered correcty');
 } else {
   alert('Oh ye of little faith ' + username + '! I have! (I even did it again the next year for some reason...)');
   console.log('Question one was answered incorrectly');
